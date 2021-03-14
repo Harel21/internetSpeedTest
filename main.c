404ee81ec435a3f69c1ac14efd76163a8932301d
Harel Dan <28harel@gmail.com>
2021-03-14 22:10:40 +0200

    finale

--- main.c
+++ main.c
@@ -4,26 +4,22 @@
 
 int main(){
 
-  char line[128]; // to read the byte from rx_bytes
-  const char PATH[] = "/sys/class/net/eno1/statistics/rx_bytes"; // path to file
+  char line[128]; // To read the byte from rx_bytes
+  const char PATH[] = "/sys/class/net/eno1/statistics/rx_bytes"; // Path to file
   FILE *fp; // rx_bytes
 
+  // Declaring vars to calculate the speed
   long long int bytes_first;
   long long int bytes_second;
   long long int result = 0;
 
-  // fgets(line, 128, fp); // reading the number from file
-  //
-  // bytes_first = atoll(line); // returns a long long int
-  // printf("first read: %lld\n", bytes_first);
-
-  for(int i = 0; i < 10; i++){
+  for(int i = 0; i < 100; i++){
     fp = fopen(PATH, "r");
     fgets(line, 128, fp);
     bytes_first = atoll(line);
     fclose(fp);
 
-    sleep(2);
+    sleep(1); // To calculate the difference
 
     fp = fopen(PATH, "r");
     fgets(line, 128, fp);
@@ -31,14 +27,13 @@ int main(){
     fclose(fp);
 
     result += bytes_second - bytes_first;
-    // printf("Your internet speed is %lld b/s\n", result);
 
     bytes_first = 0;
     bytes_second = 0;
     // result = 0;
   }
 
-  printf("your internet speed is: %lld b/sec\n", result / 10);
+  printf("your internet speed is: %lld b/sec\n", result / 100); // printing the speed
 
   return 0;
 }
